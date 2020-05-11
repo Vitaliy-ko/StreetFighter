76dd33bf007577fa432662864aaaa5b9e1cf28a3
Vitaliy_Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-05-11 19:53:16 +0300

    Create winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,25 @@
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+import { createFighterImage } from '../fighterPreview';
+import App from './../../app';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  console.dir(fighter);
+  const root = document.getElementById('root');
+  const bodyElement = createElement({ tagName: 'div', className: 'modal-body' });
+  const fighterName = createElement({ tagName: 'p', className: 'fighter-name' });
+  const fighterImage = createFighterImage(fighter);
+
+  fighterName.innerText = fighter.name;
+
+  bodyElement.append(fighterName, fighterImage);
+
+  showModal({
+    title: 'Winner',
+    bodyElement,
+    onClose: () => {
+      root.innerHTML = '';
+      new App();
+    },
+  });
 }
